24f95c4cf0af6e3701ea04182579f51e6aeabf33
Charly <charly.flu.67@gmail.com>
2024-10-30 23:05:17 +0100

    Finished with one component

--- tp3_cfl/src/app/boutique/boutique.component.ts
+++ tp3_cfl/src/app/boutique/boutique.component.ts
@@ -1,23 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { ApiService } from '../api.service';
 import { Produit } from '../models/produit';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-boutique',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './boutique.component.html',
-  styleUrl: './boutique.component.css'
+    selector: 'app-boutique',
+    standalone: true,
+    imports: [CommonModule, FormsModule],
+    templateUrl: './boutique.component.html',
+    styleUrl: './boutique.component.css'
 })
 export class BoutiqueComponent {
-  produits : Observable<Produit[]> = new Observable<Produit[]>();
+    products: Observable<Produit[]> = new Observable<Produit[]>();
+    productsFiltered: Observable<Produit[]> = new Observable<Produit[]>();
 
-  constructor(private apiService : ApiService) { }
+    differentCategories: string[] = [];
+    selectedCategory : string = "All";
 
-  ngOnInit() {
-    this.produits = this.apiService.getProduits ();
-  }
+    search : string = "";
+
+    constructor(private apiService: ApiService) { }
+
+    ngOnInit() {
+        this.products = this.apiService.getProduits();
+        this.productsFiltered = this.products;
+        this.products.subscribe(products => {
+            products.forEach(product => {
+                product.categories.forEach(category => {
+                    if (!this.differentCategories.includes(category)) {
+                        this.differentCategories.push(category);
+                    }
+                });
+            });
+        });
+    }
+
+    filterProducts() {
+        this.productsFiltered = this.products.pipe(
+            map(products => products.filter(product => {
+                if (this.selectedCategory != "All" && !product.categories.includes(this.selectedCategory)) {
+                    return false;
+                }
+                if (this.search != "" && !product.product.toLowerCase().includes(this.search.toLowerCase())) {
+                    return false;
+                }
+                
+                return true;
+            }))
+        );
+    }
 }
